d59b172e1f37cf242d9a63cb67b7ae4cf8987a32
vesmaxmacbook <jkoshchanov@gmail.com>
2024-11-03 23:12:59 -0500

    refactoring scripts for POM

---
+++ Tests/test_additional_filtering_instrument_column.py
@@ -0,0 +1,29 @@
+#TODO 4. Additional Test:
+# Identify and implement at least two more tests that you believe are important to verify for this grid.
+# Be prepared to explain your reasoning for choosing these tests.
+# For instance: filter Instrument column by Bond, Crypto, ETF, Stock
+# and return the number of rows for each filter in an array or list
+
+import unittest
+from selenium import webdriver
+from finance_grid_page import FinanceGridPage
+
+instrument_filters = ["Bond", "Crypto", "ETF", "Stock"]
+
+class TestSearchAndFilter(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.page = FinanceGridPage(self.driver)
+
+    def test_search_and_filter(self):
+       self.page.search_and_filter(instrument_filters)
+
+    def tearDown(self):
+        # Close the browser
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Tests/test_ticker_sorting.py
@@ -0,0 +1,31 @@
+#TODO 1. Create an automated end-to-end test suite for the "Finance" demo grid available at: https://www.ag-grid.com/example-finance/
+# Your test suite should verify the Sorting Functionality
+# Verify that the grid can be sorted by the "Ticker" column
+
+import unittest
+from selenium import webdriver
+from finance_grid_page import FinanceGridPage
+
+
+class TestTickerSorting(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.page = FinanceGridPage(self.driver)
+
+    def test_sort_ticker_column_descending(self):
+        self.page.sort_ticker_column_descending()
+        ticker_texts = self.page.get_column_text('ticker')
+        self.assertTrue(self.page.is_sorted_descending(ticker_texts))
+
+    def tearDown(self):
+        # Close the browser
+        self.driver.quit()
+
+
+# this helps us to run tests as a suite and better integration with CI/CD
+# this also provides clear output for each tests indicating pass/fail reasons
+if __name__ == '__main__':
+    unittest.main()
